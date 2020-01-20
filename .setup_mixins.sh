db63c95080a83fde645d47ace0b1c461fc79d891
mpowelson <matthew.powelson@swri.org>
2020-01-20 17:32:21 -0600

    WIP fix duplicate names in tesseract_Ros

---
+++ .setup_mixins.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/jrgnicho/ros_development_config/master/general/colcon_ws_setup.py
+python3 colcon_ws_setup.py
+
+# Set colcon args --symlink-install --mixin skip
