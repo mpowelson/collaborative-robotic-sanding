5e19aae0893e8b7cb12416d977b941c99428e597
mpowelson <matthew.powelson@swri.org>
2020-01-21 08:28:50 -0600

    attemp to set colcon_defaults

--- .setup_mixins.sh
+++ .setup_mixins.sh
@@ -1,4 +1,5 @@
 wget https://raw.githubusercontent.com/jrgnicho/ros_development_config/master/general/colcon_ws_setup.py
 python3 colcon_ws_setup.py
 
-# Set colcon args --symlink-install --mixin skip
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export COLCON_DEFAULTS_FILE="${DIR}/.colcon_defaults.yaml"
