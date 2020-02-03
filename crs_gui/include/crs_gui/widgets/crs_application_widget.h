02247ff0acbb38cb063686f9539b999f1901b653
mpowelson <matthew.powelson@swri.org>
2020-02-03 15:02:43 -0600

    Add state machine interface

--- crs_gui/include/crs_gui/widgets/crs_application_widget.h
+++ crs_gui/include/crs_gui/widgets/crs_application_widget.h
@@ -22,12 +22,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <crs_msgs/srv/get_available_actions.hpp>
+#include <crs_msgs/srv/execute_action.hpp>
 #include <string>
-
-class QPushButton;
-class QStateMachine;
-class QProgressBar;
-class QProgressDialog;
 
 namespace Ui
 {
@@ -52,12 +49,21 @@ protected Q_SLOTS:
 
   void onPartSelected(const std::string);
 
+  void onSMQuery();
+  void onSMApply();
+  void onSMApprove();
+  void onSMCancel();
+
 protected:
   Ui::CRSApplication* ui_;
 
   rclcpp::Node::SharedPtr node_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr current_state_sub_;
   void currentStateCB(const std_msgs::msg::String::ConstSharedPtr current_state);
+
+  rclcpp::Client<crs_msgs::srv::GetAvailableActions>::SharedPtr get_available_actions_client_;
+  rclcpp::Client<crs_msgs::srv::ExecuteAction>::SharedPtr execute_action_client_;
+
 
   PartSelectionWidget* part_selector_widget_;
   PolygonAreaSelectionWidget* area_selection_widget_;
--- crs_gui/src/widgets/crs_application_widget.cpp
+++ crs_gui/src/widgets/crs_application_widget.cpp
@@ -22,12 +22,17 @@
 #include <QProgressBar>
 #include <QProgressDialog>
 
+#include <chrono>
+
 #include <crs_gui/widgets/crs_application_widget.h>
 
 #include <crs_gui/widgets/part_selection_widget.h>
 #include <crs_gui/widgets/polygon_area_selection_widget.h>
 
 const static std::string CURRENT_STATE_TOPIC = "current_state";
+const static std::string GET_AVAILABLE_ACTIONS = "get_available_actions";
+const static std::string EXECUTE_ACTION = "execute_action";
+const static double WAIT_FOR_SERVICE_PERIOD = 1.0;
 
 namespace crs_gui
 {
@@ -46,12 +51,21 @@ CRSApplicationWidget::CRSApplicationWidget(rclcpp::Node::SharedPtr node,
   // Initialize state machine interfaces
   auto current_state_cb = std::bind(&CRSApplicationWidget::currentStateCB, this, std::placeholders::_1);
   current_state_sub_ = node_->create_subscription<std_msgs::msg::String>(CURRENT_STATE_TOPIC, 1, current_state_cb);
+  get_available_actions_client_ = node_->create_client<crs_msgs::srv::GetAvailableActions>(GET_AVAILABLE_ACTIONS);
+  execute_action_client_ = node_->create_client<crs_msgs::srv::ExecuteAction>(EXECUTE_ACTION);
+
 
   //  // Add the widgets to the UI
   ui_->vertical_layout_part_selector->addWidget(part_selector_widget_);
   ui_->vertical_layout_area_selection->addWidget(area_selection_widget_);
 
   connect(part_selector_widget_, &PartSelectionWidget::partSelected, this, &CRSApplicationWidget::onPartSelected);
+
+  // TODO: Make state machine interaction a standalone widget
+  connect(ui_->push_button_sm_apply, &QPushButton::clicked, this, &CRSApplicationWidget::onSMApply);
+  connect(ui_->push_button_sm_query, &QPushButton::clicked, this, &CRSApplicationWidget::onSMQuery);
+  connect(ui_->push_button_sm_cancel, &QPushButton::clicked, this, &CRSApplicationWidget::onSMCancel);
+  connect(ui_->push_button_sm_approve, &QPushButton::clicked, this, &CRSApplicationWidget::onSMApprove);
 }
 
 void CRSApplicationWidget::currentStateCB(const std_msgs::msg::String::ConstSharedPtr current_state)
@@ -60,5 +74,94 @@ void CRSApplicationWidget::currentStateCB(const std_msgs::msg::String::ConstShar
 }
 
 void CRSApplicationWidget::onPartSelected(const std::string selected_part) { std::cout << selected_part << std::endl; }
+
+// State machine button callbacks
+void CRSApplicationWidget::onSMApply()
+{
+  // Specify service request to get all user actions in current state
+  auto request = std::make_shared<crs_msgs::srv::ExecuteAction::Request>();
+  request->action_id = ui_->combo_box_sm_available_actions->currentText().toStdString();
+
+  // Wait if service is not available
+  if (!execute_action_client_->wait_for_service(std::chrono::duration<double>(WAIT_FOR_SERVICE_PERIOD)))
+  {
+    RCLCPP_ERROR(node_->get_logger(), "service not available");
+    return;
+  }
+  // Send request and wait for result
+  auto result = execute_action_client_->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(node_, result) != rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Failed to call service %s", GET_AVAILABLE_ACTIONS.c_str());
+  }
+}
+
+void CRSApplicationWidget::onSMQuery()
+{
+  // Specify service request to get all user actions in current state
+  auto request = std::make_shared<crs_msgs::srv::GetAvailableActions::Request>();
+  request->search_pattern = request->PATTERN_USER_ACTIONS;
+
+  // Wait if service is not available
+  if (!get_available_actions_client_->wait_for_service(std::chrono::duration<double>(WAIT_FOR_SERVICE_PERIOD)))
+  {
+    RCLCPP_ERROR(node_->get_logger(), "service not available");
+    return;
+  }
+  // Send request and wait for result
+  auto result = get_available_actions_client_->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(node_, result) == rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    // Convert ROS msg to QStringList and update combo box
+    QStringList available_actions;
+    for (std::string action : result.get()->action_ids)
+      available_actions.push_back(QString::fromUtf8(action.c_str()));
+    ui_->combo_box_sm_available_actions->addItems(available_actions);
+  }
+  else
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Failed to call service %s", GET_AVAILABLE_ACTIONS.c_str());
+  }
+}
+
+void CRSApplicationWidget::onSMCancel()
+{
+  // Specify service request to get all user actions in current state
+  auto request = std::make_shared<crs_msgs::srv::ExecuteAction::Request>();
+  request->action_id = "user_rejects";
+
+  // Wait if service is not available
+  if (!execute_action_client_->wait_for_service(std::chrono::duration<double>(WAIT_FOR_SERVICE_PERIOD)))
+  {
+    RCLCPP_ERROR(node_->get_logger(), "service not available");
+    return;
+  }
+  // Send request and wait for result
+  auto result = execute_action_client_->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(node_, result) != rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Failed to call service %s", GET_AVAILABLE_ACTIONS.c_str());
+  }
+}
+
+void CRSApplicationWidget:: onSMApprove()
+{
+  // Specify service request to get all user actions in current state
+  auto request = std::make_shared<crs_msgs::srv::ExecuteAction::Request>();
+  request->action_id = "user_approves";
+
+  // Wait if service is not available
+  if (!execute_action_client_->wait_for_service(std::chrono::duration<double>(WAIT_FOR_SERVICE_PERIOD)))
+  {
+    RCLCPP_ERROR(node_->get_logger(), "service not available");
+    return;
+  }
+  // Send request and wait for result
+  auto result = execute_action_client_->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(node_, result) != rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Failed to call service %s", GET_AVAILABLE_ACTIONS.c_str());
+  }
+}
 
 }  // namespace crs_gui
